9d2b346a0cc88be192d2319f549473c7755759e9
subbuwcm <>
2022-07-22 00:20:44 -0500

    Sling model with interface

---
+++ core/src/main/java/com/digicloud/aem/core/models/SlingModeldemoInterface.java
@@ -0,0 +1,17 @@
+package com.digicloud.aem.core.models;
+
+import java.util.List;
+
+public interface SlingModeldemoInterface {
+
+	String getTitle();
+
+	String getDescription();
+
+	String getImagePath();
+
+	String getDropwdown();
+
+	List<User> getUsers();
+
+}
---
+++ core/src/main/java/com/digicloud/aem/core/models/impl/SlingModeldemoInterfaceImpl.java
@@ -0,0 +1,65 @@
+package com.digicloud.aem.core.models.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.adobe.cq.export.json.ExporterConstants;
+import com.digicloud.aem.core.models.SlingModeldemoInterface;
+import com.digicloud.aem.core.models.User;
+
+@Model(adaptables = Resource.class, adapters = SlingModeldemoInterface.class, resourceType = "digicloud-training/components/sling-model-demo",defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class SlingModeldemoInterfaceImpl implements SlingModeldemoInterface  {
+
+	@ValueMapValue @Named("jcr:title")
+	private String title;
+	
+	@ValueMapValue @Optional
+	private String description;
+	
+	@ValueMapValue
+	@Named("fileReference")
+	private String imagePath;
+	
+	@ValueMapValue
+	private String dropwdown;
+		
+	@Inject
+    private List<User> users;
+	
+	
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	@Override
+	public String getDropwdown() {
+		return dropwdown;
+	}
+
+	@Override
+	public List<User> getUsers() {
+		return users;
+	}
+	
+}
