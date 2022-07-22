963c9df5f37b0ab4e7aa5e3f7cb0bb00ed26eceb
subbuwcm <>
2022-07-22 00:22:13 -0500

    Extending core title component by adding subheading field

---
+++ core/src/main/java/com/digicloud/aem/core/models/DCTitle.java
@@ -0,0 +1,8 @@
+package com.digicloud.aem.core.models;
+
+import com.adobe.cq.wcm.core.components.models.Title;
+
+public interface DCTitle extends Title {
+	
+	String getSubHeading();
+}
---
+++ core/src/main/java/com/digicloud/aem/core/models/impl/DCTitleImpl.java
@@ -0,0 +1,68 @@
+package com.digicloud.aem.core.models.impl;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.wcm.core.components.commons.link.Link;
+import com.adobe.cq.wcm.core.components.models.Title;
+import com.digicloud.aem.core.models.DCTitle;
+
+@Model(adaptables = {
+		SlingHttpServletRequest.class }, adapters = DCTitle.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, resourceType = "digicloud-training/components/title")
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class DCTitleImpl implements DCTitle {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Self
+	private SlingHttpServletRequest request;
+
+	@Self
+	@Via(type = ResourceSuperType.class)
+	private Title deligate;
+
+	@ValueMapValue
+	@Via("resource")
+	private String subHeading;
+
+	@PostConstruct
+	protected void initModel() {
+		logger.info("-=-==-=" + deligate + "===> Subheading : " + subHeading);
+	}
+
+	@Override
+	public String getSubHeading() {
+		return subHeading;
+	}
+
+	@Override
+	public String getText() {
+		return deligate.getText();
+	}
+
+	@Override
+	public String getType() {
+		return deligate.getType();
+	}
+
+	@Override
+	public Link getLink() {
+		return deligate.getLink();
+	}
+
+	@Override
+	public boolean isLinkDisabled() {
+		return deligate.isLinkDisabled();
+	}
+
+}
